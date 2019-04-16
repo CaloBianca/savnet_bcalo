b47778efd891b11cc527c5676923809b6f957cf0
A673215 <A673215@MC0YCQ3C.ww930.my-it-solutions.net>
2019-04-16 21:18:57 +0300

    New

---
+++ src/com/savnet/session03/MyFirstIf.java
@@ -0,0 +1,18 @@
+package com.savnet.session03;
+
+public class MyFirstIf {
+	public static void main(String[] args) {
+
+		int nota = 7;
+		if (nota > 8) {
+			System.out.println("bine");
+		} else if (nota > 6) {
+			System.out.println("satisfacator");
+		} else if (nota > 4) {
+			System.out.println("slab");
+		} else {
+			System.out.println("varza");
+		}
+	}
+
+}
--- src/com/savnet/session03/MyFisrtCommit.java
+++ src/com/savnet/session03/MyFisrtCommit.java
@@ -2,4 +2,11 @@ package com.savnet.session03;
 
 public class MyFisrtCommit {
 
+	public static void main(String[] args) {
+
+		int nota = 6;
+		System.out.println(nota > 8 ? "bine" : nota > 6 ? "satisfacator" : nota > 4 ? "slab" : "varza");
+
+	}
+
 }
