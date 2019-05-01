8de428051aa4c0fffe8b4b513c373f3f20039aa9
A673215 <A673215@MC0YCQ3C.ww930.my-it-solutions.net>
2019-05-01 21:48:33 +0300

    BA6

---
+++ src/com/savnet/session03/TestBA6.java
@@ -0,0 +1,41 @@
+package com.savnet.session03;
+
+public class TestBA6 {
+	public static void main(String[] args) {
+
+		int[] sir = {};
+		System.out.println(arrayToNumber(sir));
+	}
+
+	public static int arrayToNumber(int[] sir) {
+		int number = 0;
+		int i;
+		int newlength;
+
+		if (sir != null && sir.length != 0) {
+			for (i = 0; i < sir.length; i++) {
+				if (sir[i] > 9 || sir[i] < 0) {
+					return -2;
+				}
+
+				if (i == 0) {
+
+					number = sir[i] * (int) Math.pow(10, sir.length - 1);
+				}
+
+				else if (i == sir.length - 1) {
+
+					number += sir[i];
+				} else {
+					newlength = sir.length - 1;
+					number += sir[i] * (int) Math.pow(10, newlength - i);
+				}
+			}
+			System.out.println(number);
+			return number;
+		}
+
+		return -1;
+	}
+
+}
