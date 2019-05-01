adaa370400383145c78b701873b645e559f56a62
A673215 <A673215@MC0YCQ3C.ww930.my-it-solutions.net>
2019-05-01 19:35:10 +0300

    BA5

---
+++ src/com/savnet/session03/TestBA5.java
@@ -0,0 +1,33 @@
+package com.savnet.session03;
+
+public class TestBA5 {
+	public static void main(String[] args) {
+
+		int[] sir = null;
+		System.out.println(product(sir));
+
+	}
+
+	public static int product(int[] sir) {
+
+		int contor = 0;
+		int inmultire = 1;
+		int i = 0;
+		if (sir != null) {
+			for (i = 0; i < sir.length; i++) {
+				if (sir[i] != 0) {
+					inmultire *= sir[i];
+
+				} else {
+					contor++;
+				}
+			}
+			if (contor == sir.length) {
+				return 1;
+			}
+			System.out.println(inmultire);
+			return inmultire;
+		}
+		return 1;
+	}
+}
